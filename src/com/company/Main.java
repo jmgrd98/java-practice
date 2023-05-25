28b389010eb1e3215d526e9a31307a3453c78e49
João Marcelo Dantas <jmgrd98@gmail.com>
2023-05-25 13:54:23 -0300

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 20;
+        double height = 1.8;
+        double soma = age + height;
+        
+	System.out.println("Hello, " + name + "! You are " + age + " years old and " + height + "m tall.");
+	System.out.println("The sum of your age and height is " + soma + ".");
+    }
+}

720412f9f0546ce083a6b008afd9afeddc5cd2f6
João Marcelo Dantas <jmgrd98@gmail.com>
2023-05-25 14:06:10 -0300

    commt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,18 @@ public class Main {
         String name = "John";
         int age = 20;
         double height = 1.8;
+        double weight = 70.3;
+        double bmi = height / weight;
         double soma = age + height;
-        
-	System.out.println("Hello, " + name + "! You are " + age + " years old and " + height + "m tall.");
-	System.out.println("The sum of your age and height is " + soma + ".");
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old and " + height + "m tall.");
+        System.out.println("The sum of your age and height is " + soma + ".");
+        System.out.println("Your BMI is " + bmi + ".");
+
+        if (age < 18) {
+            System.out.println("You are not an adult.");
+        } else {
+            System.out.println("You are an adult.");
+        }
     }
 }
